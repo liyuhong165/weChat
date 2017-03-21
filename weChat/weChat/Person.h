a3d2ec38728bb2e1d9c92ba9906fd2743d42dc39
lyh165 <liyuhong165@163.com>
2017-03-21 16:50:11 +0800

    创建了Person类

---
+++ weChat/weChat/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  weChat
+//
+//  Created by liyuhong165 on 17/3/21.
+//  Copyright © 2017年 lyh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ weChat/weChat/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  weChat
+//
+//  Created by liyuhong165 on 17/3/21.
+//  Copyright © 2017年 lyh. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
